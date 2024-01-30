bc0d5f8b0051543382eac9ec76dcbb1b06e1befd
User <User@LAB2-10>
2024-01-30 14:52:43 +0200

    Add project files.

---
+++ CollectionViewMonkeys/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionViewMonkeys
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
